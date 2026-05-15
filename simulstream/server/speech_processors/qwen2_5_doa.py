d8133a9bb6e73493e89fa7c568f64160545586af
spapi <spapi@fbk.eu>
2026-05-15 20:58:16 +0200

    debug

--- simulstream/server/speech_processors/qwen2_5_doa.py
+++ simulstream/server/speech_processors/qwen2_5_doa.py
@@ -196,6 +196,9 @@ class Qwen2_5OmniDOA(DecoderOnlyAttention):
 
         first_new_row = prefill_attn[-1:, audio_positions] if new_tokens else \
             torch.zeros(0, max(audio_len, 1), device=self.device)
+        print("prefill:", output.attentions[0][0].shape)  # layer 0, prefill
+        if len(output.attentions) > 1:
+            print("step 1:", output.attentions[1][0].shape)  # layer 0, first decode step
         new_rows = [
             self.mean_attn_over_heads_and_selected_layers(step_attn).squeeze(0)[audio_positions]
             for step_attn in output.attentions[1:]
